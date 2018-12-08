15f23efaa44866f5fa34b8b842d393a62bd8714d
Shyam Grover <post2shyam@gmail.com>
2018-12-08 16:49:14 +0530

    First Commit: New app gen by AS-IDE. Blank Activity.

---
+++ app/src/main/java/com/post2shyam/abcd/FirstActivity.kt
@@ -0,0 +1,12 @@
+package com.post2shyam.abcd
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class FirstActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_first)
+  }
+}
