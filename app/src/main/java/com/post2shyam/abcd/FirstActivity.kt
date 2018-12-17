d7191a4dcb1c3e601f8de2c9e19635ace9c7d1a2
Shyam Grover <post2shyam@gmail.com>
2018-12-17 19:28:39 +0530

    Added Hockey app support.

--- app/src/main/java/com/post2shyam/abcd/FirstActivity.kt
+++ app/src/main/java/com/post2shyam/abcd/FirstActivity.kt
@@ -1,12 +1,11 @@
 package com.post2shyam.abcd
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import com.post2shyam.internal.BaseActivity
 
-class FirstActivity : AppCompatActivity() {
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_first)
-  }
+class FirstActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+    }
 }
