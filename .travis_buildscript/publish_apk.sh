bb0b221afa8c5ad7fb844193d140d5517c9f31be
Shyam Grover <post2shyam@gmail.com>
2018-12-23 11:43:06 +0530

    travis: Fixed appcenter upload script.

--- .travis_buildscript/publish_apk.sh
+++ .travis_buildscript/publish_apk.sh
@@ -2,12 +2,12 @@
 echo "Upload the apk to app center"
 
 #Issue: Gen apk upload url of appcenter
-urlRequestRspJson=$(curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' --header "X-API-Token: ${appcenter_uplaod_token}" 'https://api.appcenter.ms/v0.1/apps/post2shyam/abcd/release_uploads')
+urlRequestRspJson=$(curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' --header "X-API-Token: ${upload_token}" 'https://api.appcenter.ms/v0.1/apps/post2shyam/abcd/release_uploads')
 
 #Extract upload id from the above response
 upload_id=$(echo  ${urlRequestRspJson} | jq --raw-output '.upload_id')
 
-#Extract the uplaod url from the above response
+#Extract the upload url from the above response
 upload_url=$(echo  ${urlRequestRspJson} | jq --raw-output '.upload_url')
 
 echo ${upload_id}
@@ -23,10 +23,10 @@ release_url=$(echo  ${uploadRspJson} | jq --raw-output '.release_url')
 
 
 #Update upload resource's status to committed
-statusRsp=$(curl -X PATCH --header 'Content-Type: application/json' --header 'Accept: application/json' --header "X-API-Token: ${appcenter_uplaod_token}" -d '{ "status": "committed"  }' "https://api.appcenter.ms/v0.1/apps/post2shyam/abcd/release_uploads/${upload_id}")
+statusRsp=$(curl -X PATCH --header 'Content-Type: application/json' --header 'Accept: application/json' --header "X-API-Token: ${upload_token}" -d '{ "status": "committed"  }' "https://api.appcenter.ms/v0.1/apps/post2shyam/abcd/release_uploads/${upload_id}")
 echo $statusRsp
 
 #Distribute the uploaded release to distribution group
-relNotesRsp=$(curl -X PATCH --header 'Content-Type: application/json' --header 'Accept: application/json' --header "X-API-Token: ${appcenter_uplaod_token}" -d '{ "destination_name": "QA Testers", "release_notes": "Example new release is available" }' 'https://api.appcenter.ms/v0.1/apps/post2shyam/abcd/releases/2')
+relNotesRsp=$(curl -X PATCH --header 'Content-Type: application/json' --header 'Accept: application/json' --header "X-API-Token: ${upload_token}" -d '{ "destination_name": "QA Testers", "release_notes": "Example new release is available" }' 'https://api.appcenter.ms/v0.1/apps/post2shyam/abcd/releases/2')
 echo ${relNotesRsp}
 
