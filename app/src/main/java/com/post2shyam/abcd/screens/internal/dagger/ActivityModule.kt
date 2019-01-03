9de0948c2254f5067e693037d991513b0d62c55b
Shyam Grover <post2shyam@gmail.com>
2019-01-03 21:01:44 +0530

    Added dummy splash screen.

--- app/src/main/java/com/post2shyam/abcd/screens/internal/dagger/ActivityModule.kt
+++ app/src/main/java/com/post2shyam/abcd/screens/internal/dagger/ActivityModule.kt
@@ -1,6 +1,7 @@
 package com.post2shyam.abcd.screens.internal.dagger
 
 import com.post2shyam.abcd.screens.first.FirstActivity
+import com.post2shyam.abcd.screens.splash.SplashActivity
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
@@ -9,5 +10,8 @@ abstract class ActivityModule {
     @ContributesAndroidInjector
     internal abstract fun contributeFirstActivity(): FirstActivity
 
+
+    @ContributesAndroidInjector
+    internal abstract fun contributeSplashActivity(): SplashActivity
     // Add bindings for other sub-components here
 }
