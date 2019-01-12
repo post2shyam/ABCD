3a70d94ae5d967d685ef6ea19be39c1f63beb8a2
Shyam Grover <post2shyam@gmail.com>
2019-01-12 13:59:13 +0530

    Radio station is successfully played !!

--- app/src/main/java/com/post2shyam/abcd/screens/MainScreen/MainScreen.kt
+++ app/src/main/java/com/post2shyam/abcd/screens/MainScreen/MainScreen.kt
@@ -1,6 +1,8 @@
 package com.post2shyam.abcd.screens.MainScreen
 
 import android.content.Intent
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.os.Bundle
 import com.jakewharton.rxbinding3.view.clicks
 import com.post2shyam.abcd.R
@@ -21,12 +23,19 @@ class MainScreen : BaseActivity() {
   @Inject
   lateinit var dirbleRadioDirectoryServices: DirbleRadioDirectoryServices
 
+  private val mediaPlayer = MediaPlayer()
+
   companion object {
     fun launch(baseActivity: BaseActivity) {
       val intent = Intent(baseActivity, MainScreen::class.java)
       baseActivity.startActivity(intent)
       baseActivity.finish()
     }
+  }
+
+  override fun onDestroy() {
+    mediaPlayer.release()
+    super.onDestroy()
   }
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,12 +48,22 @@ class MainScreen : BaseActivity() {
     hello_world_tview.setOnClickListener { view -> Timber.d("%s", view.tag) }
 
     compositeDisposable.add(start_button.clicks()
-        .debounce(200, MILLISECONDS)
-        .switchMap {
-          dirbleRadioDirectoryServices.popularStations()
-              .subscribeOn(Schedulers.io())
-        }
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribe { it: Array<PopularStationsRsp>? -> Timber.d("%s", it?.get(0)?.name) })
+      .debounce(200, MILLISECONDS)
+      .switchMap {
+        dirbleRadioDirectoryServices.popularStations()
+          .subscribeOn(Schedulers.io())
+          .doOnNext { popularRadioStations: Array<PopularStationsRsp>? ->
+            Timber.d("Ok")
+            val url = popularRadioStations?.get(4)?.streams?.get(0)?.stream
+            mediaPlayer.apply {
+              setAudioStreamType(AudioManager.STREAM_MUSIC)
+              setDataSource(url)
+              prepare() // might take long! (for buffering, etc)
+              start()
+            }
+          }
+      }
+      .observeOn(AndroidSchedulers.mainThread())
+      .subscribe())
   }
 }
