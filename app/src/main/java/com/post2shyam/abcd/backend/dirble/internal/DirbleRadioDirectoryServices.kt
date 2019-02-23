5da97fc42b6f65b124dc5d56cc0e3d8df2e6bcd6
Shyam Grover <post2shyam@gmail.com>
2019-02-23 21:41:34 +0530

    Adding more dirble apis.

--- app/src/main/java/com/post2shyam/abcd/backend/dirble/internal/DirbleRadioDirectoryServices.kt
+++ app/src/main/java/com/post2shyam/abcd/backend/dirble/internal/DirbleRadioDirectoryServices.kt
@@ -1,26 +1,67 @@
 package com.post2shyam.abcd.backend.dirble.internal
 
-import com.post2shyam.abcd.backend.dirble.interactions.response.NewStationsRsp
-import com.post2shyam.abcd.backend.dirble.interactions.response.PopularStationsRsp
-import com.post2shyam.abcd.backend.dirble.interactions.response.StationsWithIdRsp
+import com.post2shyam.abcd.backend.dirble.interactions.response.*
+import com.post2shyam.abcd.backend.dirble.interactions.response.models.Category
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 const val DIRBLE_BASE_URL = "http://api.dirble.com/v2/"
 
 interface DirbleRadioDirectoryServices {
+  ///////////// STATIONS -API ////////////////////
   //Get new added stations
   @GET("stations/recent")
   fun newAddedStations(): Observable<Array<NewStationsRsp>>
 
   //Get popular stations
   @GET("stations/popular")
-  fun popularStations(): Observable<Array<PopularStationsRsp>>
+  fun popularStations(
+    @Query("page") page: Int,
+    @Query("per_page") perPage: Int,
+    @Query("offset") offset: Int
+  ): Observable<Array<PopularStationsRsp>>
 
   //Get a specific station
   @GET("station/{id}")
-  fun getStationWithId(@Path("id") id: Int): Observable<StationsWithIdRsp>
+  fun getStationWith(@Path("id") id: Int): Observable<StationsWithIdRsp>
+
+  //Get similar stations
+  @GET("station/{id}/similar")
+  fun getStationSimilarTo(@Path("id") id: Int): Observable<Array<SimilarStationsToIdRsp>>
+
+  //Get song history of station
+  @GET("station/{id}/song_history")
+  fun getSongHistoryOfStation(@Path("id") id: Int): Observable<Array<SongHistoryOfStationRsp>>
+
+  ////////////// SONGS - API   ///////////////////
+  //Get world-wide recent played songs
+  @GET("songs")
+  fun getWorldWideRecentPlayedSongs(): Observable<Array<WorldWideRecentPlayedSongsRsp>>
+
+  ////////////  CATEGORIES - API ////////////////
+  //Get all categories
+  @GET("categories")
+  fun getAllSongCategories(): Observable<Array<Category>>
+
+  //Get primary categories
+  @GET("categories/primary")
+  fun getPrimarySongCategories(): Observable<Array<Category>>
+
+  //Get child categories of
+  @GET("category/{categoryId}/childs")
+  fun getChildCategoriesOf(
+    @Path("categoryId") categoryId: Int,
+    @Query("page") page: Int,
+    @Query("per_page") perPage: Int,
+    @Query("offset") offset: Int
+  ): Observable<Array<Category>>
+
+  //Get all category tree
+  @GET("categories/tree")
+  fun getAllSongCategoriesAsTree(): Observable<Array<AllSongCategoriesAsTreeRsp>>
+
 }
 
 
