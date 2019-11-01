62a223148fc9e1c86cd7a784417e2658e7e6ebb2
Shyam Grover <post2shyam@gmail.com>
2019-11-01 11:30:58 +0530

    Identifying the pwd. For testing CI setup.

--- .travis_buildscript/version_upgrade.sh
+++ .travis_buildscript/version_upgrade.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 pwd
 ls
-sed -i '' "/VERSION_BUILD=/ s/=.*/=$TRAVIS_BUILD_NUMBER/" ./app/version.properties
+sed -i  "/VERSION_BUILD=/ s/=.*/=$TRAVIS_BUILD_NUMBER/" ./app/version.properties
